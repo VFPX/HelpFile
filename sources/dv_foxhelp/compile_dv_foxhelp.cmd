23582e7ad17199165114055bb3806f0316dd52d0
Francis FAURE <francis.faure@dord.com>
2017-08-23 17:32:00 +0200

    dv_foxhelp.chm source files added

--- sources/dv_foxhelp/compile_dv_foxhelp.cmd
+++ sources/dv_foxhelp/compile_dv_foxhelp.cmd
@@ -1,5 +1,9 @@
+del dv_foxhelp91.chm 
+del _ErrorLog.Log 
+del dv_foxhelp.log 
+del dv_foxhelp.chm 
 "C:\Program Files (x86)\HTML Help Workshop\hhc.exe" dv_foxhelp91.hhp 
-rename _ErrorLog.Log dv_foxhelp.log 
-rename dv_foxhelp91.chm dv_foxhelp.chm 
+ren _ErrorLog.Log dv_foxhelp.log 
+ren dv_foxhelp91.chm dv_foxhelp.chm 
 type dv_foxhelp.log  
 pause 
